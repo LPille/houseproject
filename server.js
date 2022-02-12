d909618d4f7ed83627a6644048b3784f175800e4
Leon Pille <leon.pille@web.de>
2022-02-12 17:22:15 +0100

    TETS

--- server.js
+++ server.js
@@ -1,42 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const request = require('request');
+const http = require('http');
+const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-    res.json({"value1":2.3, "value2":22.4, "value3":121212996.5})
-})
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello World</h1>');
+});
 
-app.get('/sendData', (req, res) => {
-    res.send('Hello World!')
-    test();
-})
-const myJSONObject = {"1":2, "2":22, "3":121212996};
-
-//Tetst
-function test(){
-    request({
-        url: "192.168.178.199/custom",
-        method: "POST",
-        json: true,   // <--Very important!!!
-        body: myJSONObject
-    }, function (error, response, body){
-        console.log(response);
-    });
-}
-
-
-/*
-request.post(
-    '192.168.178.199',
-    { json: { key: 'value' } },
-    function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            console.log(body);
-        }
-    }
-);*/
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+server.listen(port,() => {
+    console.log(`Server running at port `+port);
+});
